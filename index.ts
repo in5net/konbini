e71432200e688f3e7e7c298dea55105c1800177a
in5net <48165199+in5net@users.noreply.github.com>
2025-06-03 19:11:20 -0400

    feat: add computed stores from trkl

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 /**
  * @module konbini
  * A simple single-like reactive store that can be used in Svelte
+ * This is mainly the observables in [trkl](https://github.com/jbreckmckye/trkl) with the Svelte store contract and a few new functions
  *
  * @example
  * ```ts
- * import { konbini } from "@in5net/konbini";
+ * import { konbini, computed } from "@in5net/konbini";
  *
  * const count = konbini(0);
+ * const doubled = computed(() => count() * 2);
  */
 
 /** Called when the store's value changes */
@@ -34,6 +36,8 @@ export interface Konbini<T> {
   subscribe(subscriber: Subscriber<T>): () => void;
 }
 
+const computationStack: Subscriber<any>[] = [];
+
 /**
  * Create a new store.
  * @param value The initial value of the store
@@ -50,9 +54,17 @@ export interface Konbini<T> {
 export function konbini<T>(value?: T): Konbini<T> {
   const subscribers = new Set<Subscriber<T>>();
 
+  function read() {
+    const parentComputation = computationStack.at(-1);
+    if (parentComputation) {
+      subscribers.add(parentComputation);
+    }
+    return value as T;
+  }
+
   // @ts-expect-error
   const store: Konbini<T> = (...args) => {
-    if (!args.length) return value as T;
+    if (!args.length) return read();
 
     const oldValue = value;
     value = args[0] as T;
@@ -64,8 +76,49 @@ export function konbini<T>(value?: T): Konbini<T> {
   store.set = newValue => store(newValue);
   store.subscribe = subscriber => {
     subscribers.add(subscriber);
-    subscriber(value as T);
+    subscriber(read());
     return () => subscribers.delete(subscriber);
   };
+  return store;
+}
+
+/**
+ * Creates a computed store, where its value is derived from other stores
+ * @param executor A function that returns the value of the store
+ * @returns A store
+ *
+ * @example
+ * ```ts
+ * const count = konbini(0);
+ * const doubled = computed(() => count() * 2);
+ * count(2);
+ * doubled() // 4
+ * ```
+ */
+export function computed<T>(fn: () => T): Konbini<T> {
+  const store = konbini<T>();
+
+  function compute() {
+    if (computationStack.includes(compute)) {
+      throw new Error("Circular computation");
+    }
+    computationStack.push(compute);
+
+    let result: T;
+    let error: unknown;
+    try {
+      result = fn();
+    } catch (e) {
+      error = e;
+    }
+    computationStack.pop();
+    if (error) {
+      throw error;
+    }
+    // @ts-expect-error
+    store(result);
+  }
+  compute();
+
   return store;
 }
